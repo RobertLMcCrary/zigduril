41a72cccd116c4a59b740c9fe768a8e1aa710aab
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-08 00:44:56 -0500

    Implemented prevent reboot loop
    very shaky code these past two commits

--- arch/attiny1616.zig
+++ arch/attiny1616.zig
@@ -1,25 +1,36 @@
 const std = @import("std");
 
-const vref = @import("attiny1616+vref.zig");
 const clock = @import("attiny1616+clock.zig");
+const vref = @import("attiny1616+vref.zig");
+const wdt = @import("attiny1616+wdt.zig");
 
 //clock speed / delay stuff
 const F_CPU = 10000000;
 const BOGOMIPS = F_CPU / 4350;
 const DELAY_ZERO_TIME = 1020;
+const RSTCTRL_RSTFR = 0x0040;
+const RSTCTRL_WDRF_BITMASK = 0x08;
 
 //ADC voltage / temperature
 pub fn set_admux_therm() void {}
 
 pub fn set_admux_voltage() void {}
 
-pub fn vdd_raw2cooked(measurement: u16) u8 {}
+pub fn vdd_raw2cooked(measurement: u16) u8 {
+    _ = measurement; // autofix
+}
 
-pub fn vdd_raw2fine(measurement: u16) u16 {}
+pub fn vdd_raw2fine(measurement: u16) u16 {
+    _ = measurement; // autofix
+}
 
-pub fn vdivider_raw2cooked(measurement: u16) u8 {}
+pub fn vdivider_raw2cooked(measurement: u16) u8 {
+    _ = measurement; // autofix
+}
 
-pub fn temp_raw2cooked(measurement: u16) u16 {}
+pub fn temp_raw2cooked(measurement: u16) u16 {
+    _ = measurement; // autofix
+}
 
 //PCINT - pin change interrupt
 pub fn switch_vect_clear() void {}
@@ -28,4 +39,8 @@ pub fn pcint_off() void {}
 
 //misc
 pub fn reboot() void {}
-pub fn prevent_reboot_loop() void {}
+pub fn prevent_reboot_loop() void {
+    // prevent WDT from rebooting MCU again
+    RSTCTRL_RSTFR &= ~(RSTCTRL_WDRF_BITMASK); // reset status flag
+    wdt.disable();
+}
