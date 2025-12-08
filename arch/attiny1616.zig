a08b527fcaa354b5788baf57c873a57e8bb11f06
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-07 20:10:54 -0500

    Added more important data types

--- arch/attiny1616.zig
+++ arch/attiny1616.zig
@@ -8,6 +8,47 @@ const F_CPU = 10000000;
 const BOGOMIPS = F_CPU / 4350;
 const DELAY_ZERO_TIME = 1020;
 
+const AnalogToDigital = packed struct {
+    CTRLA: u8, // 0x00
+    CTRLB: u8, // 0x01
+    CTRLC: u8, // 0x02
+    CTRLD: u8, // 0x03
+    CTRLE: u8, // 0x04
+    SAMPCTRL: u8, // 0x05
+    MUXPOS: u8, // 0x06
+    _reserved1: u8,
+    COMMAND: u8, // 0x08
+    EVCTRL: u8, // 0x09
+    INTCTRL: u8, // 0x0A
+    INTFLAGS: u8, // 0x0B
+    DBGCTRL: u8, // 0x0C
+    TEMP: u8, // 0x0D
+    _reserved2: [2]u8,
+    RES: u16, // 0x10 (16-bit access)
+    WINLT: u16, // 0x12
+    WINHT: u16, // 0x14
+    CALIB: u8, // 0x16
+};
+
+const RealTimeCounter = packed struct {
+    CTRLA: u8, // 0x00
+    STATUS: u8, // 0x01
+    INTCTRL: u8, // 0x02
+    INTFLAGS: u8, // 0x03
+    TEMP: u8, // 0x04
+    DBGCTRL: u8, // 0x05
+    _reserved1: u8,
+    CLKSEL: u8, // 0x07
+    CNT: u16, // 0x08
+    PER: u16, // 0x0A
+    CMP: u16, // 0x0C
+    _reserved2: [2]u8,
+    PITCTRLA: u8, // 0x10
+    PITSTATUS: u8, // 0x11
+    PITINTCTRL: u8, // 0x12
+    PITINTFLAGS: u8, // 0x13
+};
+
 pub fn mcu_clock_speed() void {}
 
 //ADC voltage / temperature
