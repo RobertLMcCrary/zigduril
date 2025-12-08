28e977975bd3edbb4cb0390593d895ac07881dea
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-07 23:54:21 -0500

    Fixed assembly constraint mismatches

--- arch/attiny1616+clock.zig
+++ arch/attiny1616+clock.zig
@@ -59,10 +59,10 @@ pub const Divider = enum(u8) {
 /// See datasheet section 10.3.5 "Configuration Change Protection"
 inline fn protected_write(comptime reg_addr: u16, value: u8) void {
     asm volatile (
-        \\  out %[ccp], %[signature]
+        \\  out %i[ccp], %[signature]
         \\  sts %[reg], %[val]
         :
-        : [ccp] "n" (CCP_ADDRESS),
+        : [ccp] "n" (&CCP_ADDRESS),
           [signature] "d" (CCP_IOREG_gc),
           [reg] "n" (reg_addr),
           [val] "r" (value),
